95dd013750801000894708b66f4179065f72c9fc
SamuelRicardos <samuelric4rdo@gmail.com>
2024-12-20 00:08:37 -0300

    transicao de portfolio para o angular - parte 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full" // Garante que a rota vazia seja considerada
+},
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
